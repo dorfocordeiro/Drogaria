a03da4ea3124e94bd112256842a39d3db5103169
Rodolfo Cordeiro <270757@unifio.edu.br>
2025-06-11 16:56:29 -0300

    alterações no bean

--- src/main/java/br/com/rodolfo/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/rodolfo/drogaria/bean/EstadoBean.java
@@ -1,19 +1,43 @@
 package br.com.rodolfo.drogaria.bean;
 
-import javax.faces.application.FacesMessage;
+import br.com.rodolfo.drogaria.dao.EstadoDao;
+import br.com.rodolfo.drogaria.domain.Estado;
+import org.omnifaces.util.Messages;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
 
-@ManagedBean(name = "estadoBean")
-public class EstadoBean {
-    public void salvar(){
-        String texto = "Salvo com sucesso";
-        System.out.println("passou do salvo com sucesso");
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
-        System.out.println("passou do faces");
+@ManagedBean
+@ViewScoped
+public class EstadoBean implements Serializable {
+    private Estado estado;
 
-        FacesContext contexto = FacesContext.getCurrentInstance();
-        System.out.println("passou do context");
-        contexto.addMessage(null, message);
+    public Estado getEstado() {
+        return estado;
     }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public void novo() {
+        estado = new Estado();
+    }
+
+    public void salvar(){
+
+        try {
+            EstadoDao estadoDao = new EstadoDao();
+            estadoDao.salvar(estado);
+
+            novo();
+
+            Messages.addGlobalInfo("Estado salvo com sucesso");
+        } catch (RuntimeException erro) {
+            Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
+            erro.printStackTrace();
+        }
+    }
+
 }
